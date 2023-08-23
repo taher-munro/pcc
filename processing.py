0aa59e7b903fd171e6ade337b8dcb28c1a483aa2
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-24 03:55:24 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -5,7 +5,8 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 
 def do_calculation(prompt):
-    query = prompt
-    loader = DirectoryLoader("data/")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    return index.query(query)
+    return 'hello'
+    # query = prompt
+    # loader = DirectoryLoader("data/")
+    # index = VectorstoreIndexCreator().from_loaders([loader])
+    # return index.query(query)

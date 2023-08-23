b9625c3818af76d99f8c5554fa960261a03e07a3
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-24 03:32:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from processing import do_calculation
 
 app = Flask(__name__)
@@ -11,31 +11,9 @@ def adder_page():
         prompt = request.form["prompt"]
         if prompt is not None :
             result = do_calculation(prompt)
-            return '''
-                <html>
-                    <body>
-                       
-                         <form method="post" action=".">
-                    <p><textarea name="prompt" rows="10" cols="50" ></textarea></p>
-                    <p><input type="submit" value="Submit" /></p>
-                </form>
-                 <p>Answer: {result}</p>
-                    </body>
-                </html>
-            '''.format(result=result)
+            return render_template('screen.html',result=result)
 
-    return '''
-        <html>
-            <body>
-                {errors}
-                <p>Enter Prompt:</p>
-                <form method="post" action=".">
-                    <p><textarea name="prompt" rows="10" cols="50" ></textarea></p>
-                    <p><input type="submit" value="Submit" /></p>
-                </form>
-            </body>
-        </html>
-    '''.format(errors=errors)
+    return render_template('screen.html')
 
 if __name__ == "__main__":
     app.run()

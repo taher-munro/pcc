14c92263b3233ad25d679a8526028382351b561d
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-24 02:38:26 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,11 +1,11 @@
 import os
 import openai
+import sys
 
 from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 
 def do_calculation(prompt):
-    os.environ["OPENAI_API_KEY"] = "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"
     query = prompt
     loader = DirectoryLoader("data/")
     index = VectorstoreIndexCreator().from_loaders([loader])

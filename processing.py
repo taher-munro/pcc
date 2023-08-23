0e8601c52962a7ef23cde04e897f95e6baca4eae
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-24 02:20:00 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -5,8 +5,4 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 
 def do_calculation(prompt):
-    os.environ["OPENAI_API_KEY"] = "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"
-    query = prompt
-    loader = DirectoryLoader("data/")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    return index.query(query)
+    return "hello"

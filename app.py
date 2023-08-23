058f02f67c69caade208e693f25be397cbd564d2
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-24 01:00:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, request
+from processing import do_calculation
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route("/", methods=["GET", "POST"])
+def adder_page():
+    errors = ""
+    if request.method == "POST":
+        prompt = request.form["prompt"]
+        if prompt is not None :
+            result = do_calculation(prompt)
+            return '''
+                <html>
+                    <body>
+                       
+                         <form method="post" action=".">
+                    <p><textarea name="prompt" rows="10" cols="50" ></textarea></p>
+                    <p><input type="submit" value="Submit" /></p>
+                </form>
+                 <p>Answer: {result}</p>
+                    </body>
+                </html>
+            '''.format(result=result)
+
+    return '''
+        <html>
+            <body>
+                {errors}
+                <p>Enter Prompt:</p>
+                <form method="post" action=".">
+                    <p><textarea name="prompt" rows="10" cols="50" ></textarea></p>
+                    <p><input type="submit" value="Submit" /></p>
+                </form>
+            </body>
+        </html>
+    '''.format(errors=errors)
 
 if __name__ == '__main__':
     app.run()

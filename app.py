758f05f48be2f09b71a48eb71a12e168624aabce
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-24 18:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, request
-from processing import do_calculation
+import os
+import openai
+
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from flask import Flask, request, render_template
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
 
 app = Flask(__name__)
 
+chat_history = []
+
+def do_calculation(prompt):
+    query = prompt
+    loader = DirectoryLoader("data/")
+    index = VectorstoreIndexCreator().from_loaders([loader])
+    chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    )
+    result = chain({"question": query, "chat_history": chat_history})
+    return result['answer']
 
 @app.route("/", methods=["GET", "POST"])
 def adder_page():
@@ -11,31 +32,9 @@ def adder_page():
         prompt = request.form["prompt"]
         if prompt is not None :
             result = do_calculation(prompt)
-            return '''
-                <html>
-                    <body>
-                       
-                         <form method="post" action=".">
-                    <p><textarea name="prompt" rows="10" cols="50" ></textarea></p>
-                    <p><input type="submit" value="Submit" /></p>
-                </form>
-                 <p>Answer: {result}</p>
-                    </body>
-                </html>
-            '''.format(result=result)
+            return render_template('demo.html',result=result)
 
-    return '''
-        <html>
-            <body>
-                {errors}
-                <p>Enter Prompt:</p>
-                <form method="post" action=".">
-                    <p><textarea name="prompt" rows="10" cols="50" ></textarea></p>
-                    <p><input type="submit" value="Submit" /></p>
-                </form>
-            </body>
-        </html>
-    '''.format(errors=errors)
+    return render_template('demo.html')
 
 if __name__ == "__main__":
     app.run()

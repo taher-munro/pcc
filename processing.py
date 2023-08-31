eefc35599ec1d4d1703f3376b22d22588fb4eb4c
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-08-31 21:13:47 +0530

    Delete processing.py

--- processing.py
+++
@@ -1,8 +0,0 @@
-
-
-def do_calculation(prompt):
-    os.environ["OPENAI_API_KEY"] = ""
-    query = prompt
-    loader = DirectoryLoader("data/")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    return index.query(query)

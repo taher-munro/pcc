052ac0f36c70580d4df20cf21dadda3dee93efad
taher-munro <141963946+taher-munro@users.noreply.github.com>
2023-09-01 01:59:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 import os
 import openai
-import pandas
 
-from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
 from flask import Flask, request, render_template
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
-from langchain.chat_models import ChatOpenAI, ChatAnthropic
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.llms import OpenAI
 from langchain.vectorstores import Chroma
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.schema import HumanMessage
-from langchain.callbacks.base import BaseCallbackHandler
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 import requests
 
 app = Flask(__name__, template_folder='templates')
 
 chat_history = []
+PERSIST = True
 
 
 
 def do_calculation(prompt):
     query = prompt
-    #loader = DirectoryLoader("data/")
-    loader = TextLoader("data/data.txt")
-    index = VectorstoreIndexCreator().from_loaders([loader])
+    if PERSIST and os.path.exists("persist"):
+        print("Reusing index...\n")
+        vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+        index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    else:
+        loader = DirectoryLoader("data/")
+        if PERSIST:
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+        else:
+            index = VectorstoreIndexCreator().from_loaders([loader])
     chain = ConversationalRetrievalChain.from_llm(
     llm=ChatOpenAI(model="gpt-3.5-turbo",  temperature=0),
     retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
